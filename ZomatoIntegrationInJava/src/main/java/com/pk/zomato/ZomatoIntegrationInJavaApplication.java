a2f8ed42c635b82252d54c29428c7b40ff7ca284
nswarnalatha <nswarnalatha@nswarnalatha-lp.pkrmsezdc.com>
2018-12-20 20:56:40 +0530

    for

---
+++ ZomatoIntegrationInJava/src/main/java/com/pk/zomato/ZomatoIntegrationInJavaApplication.java
@@ -0,0 +1,14 @@
+package com.pk.zomato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoIntegrationInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoIntegrationInJavaApplication.class, args);
+	}
+
+}
+
